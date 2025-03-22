70f2bc0089ace3079014beaae7000d0fc3b937c3
Olivera2708 <olivera.radovanovic2002@gmail.com>
2025-03-22 19:15:08 +0100

    hard shadows

---
+++ Assets/Scripts/LightObject.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public struct LightObject
+{
+    public int type;
+    public Vector3 position;
+    public Vector3 direction;
+    public Vector4 color;
+    public float intensity;
+}
--- Assets/Scripts/RayTracingManager.cs
+++ Assets/Scripts/RayTracingManager.cs
@@ -13,12 +13,14 @@ public class RayTracingManager : MonoBehaviour
     private ComputeBuffer materialBuffer;
     private ComputeBuffer materialIndexBuffer;
     private ComputeBuffer bvhBuffer;
+    private ComputeBuffer lightBuffer;
     private RenderTexture accumTexture;
     
     private List<int> materialIndices = new List<int>();
     private List<Vector3> materialColors = new List<Vector3>();
     private List<Vector3> vertices = new List<Vector3>();
     private List<int> indices = new List<int>();
+    private List<LightObject> lightObjects = new List<LightObject>();
     private int[] hitResults;
     private int triangleCount;
     
@@ -34,11 +36,45 @@ public class RayTracingManager : MonoBehaviour
         rayTracingCamera = Camera.main;
         GetSceneObjects();
         GetMaterials();
+        GetLights();
         
         bvh = new BVH(vertices, indices);
         
         InitComputeBuffers();
         InitRenderTexture();
+    }
+    
+    private int GetLightTypeValue(LightType type)
+    {
+        switch (type)
+        {
+            case LightType.Directional: return 0;
+            case LightType.Point:       return 1;
+            default:        return 2;
+        }
+    }
+
+    void GetLights()
+    {
+        Light[] lights = FindObjectsOfType<Light>();
+
+        foreach (var light in lights)
+        {
+            Vector3 dir = light.type is LightType.Directional or LightType.Spot
+                ? -light.transform.forward
+                : Vector3.zero;
+            
+            LightObject lightObject = new LightObject
+            {
+                type = GetLightTypeValue(light.type),
+                position = light.transform.position,
+                direction = dir,
+                color = light.color,
+                intensity = light.intensity
+            };
+            
+            lightObjects.Add(lightObject);
+        }
     }
 
     void GetMaterials()
@@ -60,28 +96,29 @@ public class RayTracingManager : MonoBehaviour
         indices.Clear();
         materialIndices.Clear();
         int objectIndex = 0;
-
+    
         MeshRenderer[] meshes = FindObjectsOfType<MeshRenderer>();
         foreach (var mesh in meshes)
         {
             MeshFilter meshFilter = mesh.GetComponent<MeshFilter>();
             if (meshFilter == null || meshFilter.sharedMesh == null) continue;
-
+    
             Mesh meshData = meshFilter.sharedMesh;
             int vertexOffset = vertices.Count;
-
+    
             foreach (Vector3 vertex in meshData.vertices)
                 vertices.Add(mesh.transform.TransformPoint(vertex));
-
+    
             foreach (int index in meshData.triangles)
             {
                 indices.Add(index + vertexOffset);
                 materialIndices.Add(objectIndex);
             }
-
+    
             objectIndex++;
         }
     }
+
 
     void InitRenderTexture()
     {
@@ -108,12 +145,14 @@ public class RayTracingManager : MonoBehaviour
         if (materialBuffer != null) materialBuffer.Release();
         if (materialIndexBuffer != null) materialIndexBuffer.Release();
         if (bvhBuffer != null) bvhBuffer.Release();
+        if (lightBuffer != null) lightBuffer.Release();
     
         vertexBuffer = new ComputeBuffer(vertices.Count, sizeof(float) * 3);
         indexBuffer = new ComputeBuffer(indices.Count, sizeof(int));
         materialBuffer = new ComputeBuffer(materialColors.Count, sizeof(float) * 3);
         materialIndexBuffer  = new ComputeBuffer(materialIndices.Count, sizeof(int));
         bvhBuffer = new ComputeBuffer(bvh.nodes.Count, sizeof(int) * 5 + sizeof(float) * 6);
+        lightBuffer = new ComputeBuffer(lightObjects.Count, sizeof(int) + sizeof(float) * 11);
     
         int totalPixels = Screen.width * Screen.height;
         triangleCount = indices.Count / 3;
@@ -123,6 +162,7 @@ public class RayTracingManager : MonoBehaviour
         materialBuffer.SetData(materialColors);
         materialIndexBuffer.SetData(materialIndices);
         bvhBuffer.SetData(bvh.nodes);
+        lightBuffer.SetData(lightObjects);
         hitResults = new int[totalPixels];
         
         if (frustumBuffer != null) frustumBuffer.Release();
@@ -154,11 +194,13 @@ public class RayTracingManager : MonoBehaviour
         rayTracingShader.SetInt("width", Screen.width);
         rayTracingShader.SetInt("height", Screen.height);
         rayTracingShader.SetInt("triangleCount", triangleCount);
+        rayTracingShader.SetInt("lightCount", lightObjects.Count);
         rayTracingShader.SetBuffer(0, "vertices", vertexBuffer);
         rayTracingShader.SetBuffer(0, "indices", indexBuffer);
         rayTracingShader.SetBuffer(0, "materialColors", materialBuffer);
         rayTracingShader.SetBuffer(0, "materialIndices", materialIndexBuffer);
         rayTracingShader.SetBuffer(0, "bvhNodes", bvhBuffer);
+        rayTracingShader.SetBuffer(0, "lights", lightBuffer);
         
         rayTracingShader.SetVector("cameraPosition", rayTracingCamera.transform.position);
         rayTracingShader.SetVector("cameraRight", rayTracingCamera.transform.right);
@@ -212,5 +254,6 @@ public class RayTracingManager : MonoBehaviour
         if (materialBuffer != null) materialBuffer.Release();
         if (materialIndexBuffer != null) materialIndexBuffer.Release();
         if (bvhBuffer != null) bvhBuffer.Release();
+        if (lightBuffer != null) lightBuffer.Release();
     }
 }
